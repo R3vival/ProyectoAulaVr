931c35a226389532985fc6323707aee3becc5a99
R3vival <elsufra2012@gmail.com>
2018-10-01 23:45:54 -0500

    Basic UI

---
+++ Assets/UdemVirtual/Scripts/Place.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Place")]
+public class Place : ScriptableObject {
+
+    public string Nombre="Nombre del lugar";
+    public int Bloque;
+    public Place padre;
+
+    public GameObject Model;
+
+
+	
+}
