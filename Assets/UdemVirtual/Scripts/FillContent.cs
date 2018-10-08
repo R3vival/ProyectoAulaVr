d27ccf43fe28c879c6980292cbfb2f05860be3d8
R3vival <elsufra2012@gmail.com>
2018-10-08 13:10:27 -0500

    SomeChanges

--- Assets/UdemVirtual/Scripts/FillContent.cs
+++ Assets/UdemVirtual/Scripts/FillContent.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class FillContent : MonoBehaviour {
 
     public PlaceList placeList;
-    
 
+    public List<Place> PpalList;
 
     private void Start()
     {
@@ -29,14 +29,29 @@ public class FillContent : MonoBehaviour {
 
         }
     }
-
     public void SetDefaultValues(GameObject placeGO, Place place)
     {
         placeGO.gameObject.transform.Find("Text").GetComponent<Text>().text = place.name;
     }
+    public void Search(Text text)
+    {
+        GameObject y = null;
+        foreach (Place x in placeList.placesList)
+        {
+            Debug.Log("Buscando");
+            if (x.name.Contains(text.text))
+            {
+                Debug.Log("Encontre");
+                RefillContent(x,y);
+            }
 
-    public void SetContentCellSize()
-    {        
-        //transform.GetComponentInParent<CanvasScaler>();
+        }
     }
+    public void RefillContent(Place GO,GameObject GOHelper){
+        GOHelper = Instantiate(GO.Model, transform.position, transform.rotation) as GameObject;
+        GOHelper.transform.SetParent(transform);
+        GOHelper.transform.localScale = new Vector3(1, 1, 1);
+        SetDefaultValues(GOHelper, GO);
+    }
+
 }
